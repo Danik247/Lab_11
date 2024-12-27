4086a7667d2d92b0e10e85ed6efa317bad0ce648
mac <kazorind@gmail.com>
2024-12-27 14:06:03 +0300

    Initial commit

--- src/persons/Goat.java
+++ src/persons/Goat.java
@@ -4,14 +4,16 @@ import helpfulClasses.Console;
 
 public class Goat extends Person {
 
+    public Goat(float dexterity, float speed, float stamina, float eloquence) {
+        super("Козел", dexterity, speed, stamina, eloquence, 2, 3);
+    }
+
     @Override
     void boringStart() {
         Console.describe("    "+name + ": А почему мне дали только ловкость?\n    XXXX: Тихо, мы начали");
     }
 
-    public Goat(float dexterity, float speed, float stamina, float eloquence) {
-        super("Козел", dexterity, speed, stamina, eloquence, 2, 3);
-    }
+
 
     public Goat(float dexterity){
         float speed = (float) (dexterity*1.5);
--- src/persons/Man.java
+++ src/persons/Man.java
@@ -4,14 +4,15 @@ import helpfulClasses.Console;
 
 public class Man extends Person {
 
+    public Man(String name, float dexterity, float speed, float stamina, float eloquence) {
+        super(name, dexterity, speed, stamina, eloquence, 3, 5);
+    }
+
     @Override
     void boringStart() {
         Console.describe("    " + name + ": только ловкость?!\n    XXXX: А ну тихо, мы начинаем");
     }
 
-    public Man(String name, float dexterity, float speed, float stamina, float eloquence) {
-        super(name, dexterity, speed, stamina, eloquence, 3, 5);
-    }
 
     public Man(String name, float dexterity) {
         float stamina = dexterity * 4;
